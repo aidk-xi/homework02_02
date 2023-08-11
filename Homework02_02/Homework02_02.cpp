1a2d9ae5c69f943c71df971b0472e5c5d881e70b
Горячева Елизавета Александровна <goryacheva-ea@ranepa.ru>
2023-08-11 17:51:53 +0300

    Initial

---
+++ Homework02_02/Homework02_02.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct account 
+{
+
+	int Number;
+	std::string Name;
+	long double Balance;
+
+};
+
+void BalanceChanging(account& person)
+{
+	std::cout << "Введите новый баланс: "; std::cin >> person.Balance;
+}
+
+void print(account& person)
+{
+	std::cout << "Ваш счет: " << person.Number << ", " << person.Name << ", " << person.Balance;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+
+	account person;
+	
+	std::cout << "Введите номер счета: "; std::cin >> person.Number;
+
+	std::cout << "Введите имя: "; std::cin >> person.Name;
+
+	std::cout << "Введите баланс: "; std::cin >> person.Balance;
+
+	BalanceChanging(person);
+	print(person);
+
+	return 0;
+}
+
+
